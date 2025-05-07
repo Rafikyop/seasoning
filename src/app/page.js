352aefefa15d6258115c277837cf60e71980a5f8
Rafiky <rafikyop@gmail.com>
2025-05-06 22:41:22 -0500

    Primer commit - configurado Tailwind CSS

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-blue-500 flex items-center justify-center">
+      <h1 className="text-white text-4xl">¡Hola, mundo con Tailwind!</h1>
+    </div>
+  );
+}
